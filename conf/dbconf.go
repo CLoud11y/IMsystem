6b6cd7bc2b30b873e8fe8ac97b3e348a4b70ac59
CLoud11y <963165605@qq.com>
2023-11-15 15:06:03 +0800

    load env variable for db

--- conf/dbconf.go
+++ conf/dbconf.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,18 +13,23 @@ var MysqlConf gorm.Dialector
 var GormConf gorm.Option
 
 func init() {
+	err := godotenv.Load("conf/db.env")
+	if err != nil {
+		fmt.Println("load db.env err:", err)
+	}
+
 	//配置MySQL连接参数
-	username := "root"               //账号
-	password := os.Getenv("MYSQLPW") //密码
-	host := "8.130.86.24"            //数据库地址，可以是Ip或者域名
-	port := 3306                     //数据库端口
-	Dbname := "IMsystem"             //数据库名
+	username := os.Getenv("MYSQLNAME") //账号
+	password := os.Getenv("MYSQLPW")   //密码
+	host := os.Getenv("HOST")          //数据库地址，可以是Ip或者域名
+	port := os.Getenv("PORT")          //数据库端口
+	Dbname := os.Getenv("DBNAME")      //数据库名
 
 	if password == "" {
 		fmt.Println("the database password is empty!")
 	}
 	//拼接dsn参数
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, Dbname)
 	fmt.Println("DSN:", dsn)
 
 	MysqlConf = mysql.New(mysql.Config{
